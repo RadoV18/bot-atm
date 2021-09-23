2a9c285b4b45fac53367090d806dd75ea1c9dee5
Radomir Vladislavic <vladislavic.rl@gmail.com>
2021-09-23 13:08:21 -0400

    Creando proyecto con maven

---
+++ src/main/java/bo/edu/ucb/est/App.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.est;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
